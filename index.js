cfc14388d93a8a1d27f5cbd2e0b3ac780691751f
Jeffrey Cohetero <jeffreycoheter0@gmail.com>
2025-09-30 13:01:29 -0400

    completed outfit functions

--- index.js
+++ index.js
@@ -8,6 +8,16 @@ Below 40 → "Wear a heavy coat, scarf, and boots."
 
 const outfitPicker = (temp) => {
   // ✍️ Write your code here
+  if (temp >= 80) {
+    return ("Wear shorts and a t-shirt.");
+  } else if (temp >= 60) {
+    return ("Wear jeans and a light jacket.");
+  } else if (temp >= 40) {
+    return ("Wear a sweater and pants.");
+  } else {
+    return ("Wear a heavy coat, scarf, and boosts.");
+  }
 };
 
 //Test your function below
+console.log(outfitPicker(25));
